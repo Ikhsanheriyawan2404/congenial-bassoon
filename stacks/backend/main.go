c7287a4480df087c948a335b4ecfa18961cb8f58
ikhsanheriyawan2404 <ikhsanheriyawan2404@gmail.com>
2024-03-10 22:24:18 +0700

    add ...

--- stacks/backend/main.go
+++ stacks/backend/main.go
@@ -1,88 +1,39 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+	"fiber/app"
+	"fiber/helper"
+	"fiber/model"
+	"github.com/gofiber/fiber/v2"
 	"time"
-
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 )
 
-func connect() (*sql.DB, error) {
-	bin, err := ioutil.ReadFile("/run/secrets/db-password")
-	if err != nil {
-		return nil, err
-	}
-	return sql.Open("postgres", fmt.Sprintf("postgres://postgres:%s@db:5432/example?sslmode=disable", string(bin)))
-}
-
-func blogHandler(w http.ResponseWriter, r *http.Request) {
-	db, err := connect()
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT title FROM blog")
-	if err != nil {
-		w.WriteHeader(500)
-		return
-	}
-	var titles []string
-	for rows.Next() {
-		var title string
-		err = rows.Scan(&title)
-		titles = append(titles, title)
-	}
-	json.NewEncoder(w).Encode(titles)
-}
-
 func main() {
-	log.Print("Prepare db...")
-	if err := prepare(); err != nil {
-		log.Fatal(err)
-	}
+	server := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+		Prefork:      true,
+	})
 
-	log.Print("Listening 8000")
-	r := mux.NewRouter()
-	r.HandleFunc("/", blogHandler)
-	log.Fatal(http.ListenAndServe(":8000", handlers.LoggingHandler(os.Stdout, r)))
-}
+	config := app.Config()
+	var db = app.DatabaseInit(config)
 
-func prepare() error {
-	db, err := connect()
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+	server.Get("/api/hello", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
 
-	for i := 0; i < 60; i++ {
-		if err := db.Ping(); err == nil {
-			break
+	server.Get("/api/gorm", func(ctx *fiber.Ctx) error {
+		var todos []model.Todo
+		err := db.Find(&todos).Error
+
+		if err != nil {
+			return ctx.Status(500).SendString(err.Error())
 		}
-		time.Sleep(time.Second)
-	}
 
-	if _, err := db.Exec("DROP TABLE IF EXISTS blog"); err != nil {
-		return err
-	}
+		return ctx.JSON(todos)
+	})
 
-	if _, err := db.Exec("CREATE TABLE IF NOT EXISTS blog (id SERIAL, title VARCHAR)"); err != nil {
-		return err
-	}
-
-	for i := 0; i < 5; i++ {
-		if _, err := db.Exec("INSERT INTO blog (title) VALUES ($1);", fmt.Sprintf("Blog post #%d", i)); err != nil {
-			return err
-		}
-	}
-	return nil
+	err := server.Listen("localhost:4001")
+	helper.PanicIfError(err)
 }
